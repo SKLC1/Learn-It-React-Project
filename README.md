7e0521bbe87e7ea820c3d41b77a7078b1778f6b2
SKLC1 <segevklein2000@gmail.com>
2022-05-31 20:45:16 +0300

    initial files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Getting Started with Create React A# Learn-It-React-Project
+
---
+++ src/components/Explore/Explore.jsx
@@ -0,0 +1,11 @@
+
+
+function ExplorePage() {
+  return ( 
+    <div>
+      im explore
+    </div>
+   )
+}
+
+export default ExplorePage;
