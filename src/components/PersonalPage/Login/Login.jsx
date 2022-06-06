effb95aed33149c69ab880d5c97449b6ba229a33
SKLC1 <segevklein2000@gmail.com>
2022-06-06 22:44:06 +0300

    styling

--- src/components/PersonalPage/Login/Login.jsx
+++ src/components/PersonalPage/Login/Login.jsx
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { app, authentication } from "../../../firebase/firebase";
 import { UserContext } from "../../UserContext/UserContext";
+import './login.css'
 
 function Login() {
   const currentUser = useContext(UserContext)
@@ -13,14 +14,18 @@ function Login() {
   return ( 
     <div>
       { currentUser &&<p>logged in as {currentUser.displayName}</p>}
-      <div>
+      <div className="flexCol">
+        <label>Email:</label>
        <input type="text" />
+        <label>Password:</label>
        <input type="text" />
-       <button>Login</button>
-       <button onClick={googleLogin}>Login With Google</button>
+       <div className="flex">
+        <button className="button-28 loginBtn">Login</button>
+        <button className="button-28 loginBtn" onClick={googleLogin}>Login With Google</button>
+       </div>
+       <div>Dont have an account?</div> 
+       <Link to="/signup">Create an account</Link>
       </div>
-      <div>Dont have an account?</div> 
-      <Link to="/signup">Create an account</Link>
     </div>
    );
 }
