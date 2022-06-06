173f0e3680a797df740e0fc4bcfa3e5ba055f2bd
SKLC1 <segevklein2000@gmail.com>
2022-06-06 15:41:26 +0300

    show user at header

--- src/components/Utility/Header.jsx
+++ src/components/Utility/Header.jsx
@@ -1,16 +1,17 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../UserContext/UserContext';
 import './utility.css'
 
 function Header() {
+  const currentUser = useContext(UserContext)
   return ( 
     <div>
       <div className="header-cont">
         <div>
           Logo
         </div>
-        <div>
-          <Link to='/login'>Login/account page</Link>
-        </div>
+        {currentUser?currentUser.displayName:<div><Link to='/login'>Login/account page</Link></div>}
       </div>
     </div>
    );
