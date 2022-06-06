e3aa288b76bc8bdaf0bc65ab3972958f964de726
SKLC1 <segevklein2000@gmail.com>
2022-06-06 12:56:46 +0300

    added upload page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Category from "./components/Explore/Category/category";
 import ExplorePage from "./components/Explore/Explore";
 import SubCategory from "./components/Explore/SubCategory/subCategory";
 import Login from "./components/PersonalPage/Login/Login";
+import UploadVideo from "./components/PersonalPage/UploadVideo/UploadVideo";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
          <Route path="/category:chosenCategory" element={<Category/>}/>
          <Route path="/subcategory:chosenSubCategory" element={<SubCategory/>}/>
          <Route path="/login" element={<Login/>}/> {/* if !account, else show signup */}
+         <Route path="/upload" element={<UploadVideo/>}/>
         </Routes>
       </Router>
     </div>
--- src/components/PersonalPage/UploadVideo/UploadVideo.jsx
+++ src/components/PersonalPage/UploadVideo/UploadVideo.jsx
@@ -1,8 +1,56 @@
+import { async } from "@firebase/util";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { useState } from "react";
+import { app, db, storage } from "../../../firebase/firebase";
 
 function UploadVideo() {
+  const [progress, setProgress] = useState(0)
+  const [fileUrl, setFileUrl] = useState("")
+  const [category, setCategory] = useState()
+  const [subCategory, setSubCategory] = useState()
+  function formHandler(e){
+    e.preventDefault();
+    const file = e.target[2].files[0];
+    uploadFiles(file)
+  }
+
+  function uploadFiles(file){
+    if(!file) return;
+    const storageRef = ref(storage, `/demoVideos/${file.name}`)
+    const uploadTask = uploadBytesResumable(storageRef, file)
+    uploadTask.on('state_changed', (snapshot)=>{
+      const prog = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+      setProgress(prog)
+    }, (err) => console.log(err), 
+    ()=> {
+      getDownloadURL(uploadTask.snapshot.ref)
+      .then((url)=>{
+        setFileUrl(url)
+        console.log(fileUrl)
+        addToDataBase(fileUrl)
+      })
+    })
+  }
+  async function addToDataBase(url) {
+    const collectionRef = collection(db,'Videos');
+    const payload = {category: [category], subCategory: [subCategory], user: '', videoURL: [url]} 
+    const docRef = await addDoc(collectionRef, payload)
+    console.log(docRef.id)
+  }
   return ( 
     <div>
-      upload here
+      <h2>Upload your Video</h2>
+      <form onSubmit={formHandler}>
+        <label>Category:</label>
+       <input type='text' onChange={(e)=>setCategory(e.target.value)}/>
+        <label>SubCategory:</label>
+       <input type='text' onChange={(e)=>setSubCategory(e.target.value)}/>
+       <input type='file'/>
+       <button type="submit">Upload</button>
+      </form>
+      <h3> Uploaded {progress}%</h3>
+
     </div>
    )
 }
